e5ade172229ba9cc070fe10d555f3d0fb8bf4e2b
r0m21 <romainwenger21@gmail.com>
2018-05-12 15:25:32 +0200

    Ajout du framework owl-carousel + intégration html terminé + ajout de plusieurs classes css + ajout de fonction js pour faire un carrousel.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-$('.carousel').carousel();
+$(document).ready(function(){
+
+    $('.carousel').carousel();
+
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive:{
+            0:{
+                items: 1
+            },
+            768:{
+                items: 2
+            }
+        }
+
+    });
+
+  });
