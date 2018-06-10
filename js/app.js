ad2495ec5eccf213785ce268ea2f7230e2fa6be6
r0m21 <romainwenger21@gmail.com>
2018-06-10 17:44:11 +0200

    Refonte du header html/css, ajout de script pour crée une sticky navbar, reste à ajouter la fonctionnalité 'active' en fonction de la section où on se trouve.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
+    // Init carousel bootstrap.
     $('.carousel').carousel();
 
+    // Réglages du carousel (owl-carousel) + responsive.
     $(".owl-carousel").owlCarousel({
         items: 2,
         loop: true,
@@ -16,4 +18,32 @@ $(document).ready(function(){
             }
         }
     });
+
+    $('body').scrollspy({ target: '#navigation' });
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+      });
+    console.log($('body').scrollspy({ target: '#navigation' }));
+
+    // Smooth Scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+	// On Scroll
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+
+		// Fixed nav
+        wScroll > 1 ? $('#navigation').addClass('fixed-top justify-content-around sticky-nb-bg') : $('#navigation').removeClass('fixed-top justify-content-around sticky-nb-bg');
+        wScroll > 1 ? $('.nav-link').addClass('sticky-nb-liens') : $('.nav-link').removeClass('sticky-nb-liens');
+        wScroll > 1 ? $('#logo').attr('src', 'img/logo.png') : $('#logo').attr('src', 'img/logo-alt.png');
+                     
+	});
   });
